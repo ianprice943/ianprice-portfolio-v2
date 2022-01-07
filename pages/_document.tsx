87ed3dda5e5ebd68c7d8762a01e9020f2fb081f8
Ian Price <ianprice943@yahoo.com>
2022-01-07 11:37:56 -0500

    removed commented function code from _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,20 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
-
-// const MyDocument: Document = () => {
-    
-    
-//     return (
-//         <Html lang="en" prefix="og: http://ogp.me/ns#">
-//             <Head />
-//             <body>
-//                 <Main />
-//                 <NextScript />
-//             </body>
-//         </Html>
-//     );
-// }
-
 class MyDocument extends Document {
     render() {
         return (
